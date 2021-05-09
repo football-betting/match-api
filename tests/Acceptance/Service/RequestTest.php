c1a96246634eb77ee36207fcc90313db372c5a22
Kai <k.urban@nexus-united.com>
2021-05-09 21:08:05 +0200

    added test with live flag

--- tests/Acceptance/Service/RequestTest.php
+++ tests/Acceptance/Service/RequestTest.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 class RequestTest extends KernelTestCase
 {
     private const TOTAL_MATCHES_GROUP_STAGE = 36;
+    private const COMPETITION_NAME = 'European Championship';
 
     public function testSuccessfulWithoutLiveFlag(): void
     {
@@ -20,5 +21,17 @@ class RequestTest extends KernelTestCase
 
         self::assertSame(self::TOTAL_MATCHES_GROUP_STAGE, $competitionDataProvider->getCount());
         self::assertCount(self::TOTAL_MATCHES_GROUP_STAGE, $competitionDataProvider->getMatches());
+    }
+
+    public function testWithLiveFlag()
+    {
+        self::bootKernel();
+
+        /** @var Request $request */
+        $request = self::$container->get(Request::class);
+
+        $competitionDataProvider = $request(true);
+
+        self::assertSame(self::COMPETITION_NAME, $competitionDataProvider->getCompetition()->getName());
     }
 }
